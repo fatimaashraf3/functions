4aa27c00fb9e2d5eb5a09a37acb2ab3df19ade48
fatimaaahraf <143636229+fatimaaahraf@users.noreply.github.com>
2024-05-21 01:24:20 +0500

    push

---
+++ script.js
@@ -0,0 +1,130 @@
+function a(){
+    document.write("<center>")
+    document.write("Aeroplane"+ "<br>" + "Animal" + "<br>" + "Apple" + "<br>" + "Angel")
+    document.write("</center>")
+}
+function b(){
+    document.write("<center>")
+    document.write("Ball"+ "<br>" + "Basket" + "<br>" + "Banana" + "<br>"+ "Botany")
+    document.write("</center>")
+}
+function c(){
+    document.write("<center>")
+    document.write("Cinemon"+ "<br>" + "Carrot" + "<br>" + "Corriander" + "<br>"+ "Chemistry")
+    document.write("</center>")
+}
+function d(){
+    document.write("<center>")
+    document.write("Duck"+ "<br>" + "Doll" + "<br>" + "Door" + "<br>"+ "Doll")
+    document.write("</center>")
+}
+function e(){
+    document.write("<center>")
+    document.write("Elephant"+ "<br>" + "Euglena" + "<br>" + "Egg" + "<br>"+ "Eye")
+    document.write("</center>")
+}
+function f(){
+    document.write("<center>")
+    document.write("Fan"+ "<br>" + "Food" + "<br>" + "Fossil fuel" + "<br>"+ "Fairy")
+    document.write("</center>")
+}
+function g(){
+    document.write("<center>")
+    document.write("Gorrilla"+ "<br>" + "Giraf" + "<br>" + "Gut feeling" + "<br>"+ "Gun")
+    document.write("</center>")
+}
+function h(){
+    document.write("<center>")
+    document.write("House"+ "<br>" + "Horse" + "<br>" + "Hello" + "<br>"+ "Hand")
+    document.write("</center>")
+}
+function i(){
+    document.write("<center>")
+    document.write("Ink"+ "<br>" + "Ideal" + "<br>" + "Ice" + "<br>"+ "Identification")
+    document.write("</center>")
+}
+function j(){
+    document.write("<center>")
+    document.write("Jug"+ "<br>" + "Jacket" + "<br>" + "Jaw" + "<br>"+ "Jealous")
+    document.write("</center>")
+}
+function k(){
+    document.write("<center>")
+    document.write("Key"+ "<br>" + "Kitten" + "<br>" + "Kite" + "<br>"+ "Kiwi")
+    document.write("</center>")
+}
+function l(){
+    document.write("<center>")
+    document.write("List"+ "<br>" + "Light" + "<br>" + "Lavendar" + "<br>"+ "Lock")
+    document.write("</center>")
+}
+function m(){
+    document.write("<center>")
+    document.write("Mocha frappe"+ "<br>" + "Mask" + "<br>" + "Mint" + "<br>"+ "Magzine")
+    document.write("</center>")
+}
+function n(){
+    document.write("<center>")
+    document.write("Newborn"+ "<br>" + "Nature" + "<br>" + "Nap" + "<br>"+ "Nails")
+    document.write("</center>")
+}
+function o(){
+    document.write("<center>")
+    document.write("Owl"+ "<br>" + "Owner" + "<br>" + "Oath" + "<br>"+ "Olx")
+    document.write("</center>")
+}
+function p(){
+    document.write("<center>")
+    document.write("Petrol"+ "<br>" + "Pizza" + "<br>" + "Physics" + "<br>"+ "Pasta")
+    document.write("</center>")
+}
+function q(){
+    document.write("<center>")
+    document.write("Quiz"+ "<br>" + "Question" + "<br>" + "Queen" + "<br>"+ "Qualify")
+    document.write("</center>")
+}
+function r(){
+    document.write("<center>")
+    document.write("Ruritania"+ "<br>" + "Rubella" + "<br>" + "Roar" + "<br>"+ "Row")
+    document.write("</center>")
+}
+function s(){
+    document.write("<center>")
+    document.write("Song"+ "<br>" + "Sawat" + "<br>" + "Sushi" + "<br>"+ "Symbol")
+    document.write("</center>")
+}
+function t(){
+    document.write("<center>")
+    document.write("Throw"+ "<br>" + "Think" + "<br>" + "Tank" + "<br>" +"Torch")
+    document.write("</center>")
+}
+function u(){
+    document.write("<center>")
+    document.write("Uncle"+ "<br>" + "Umbrella" + "<br>" + "USA" + "<br>" +"Universe")
+    document.write("</center>")
+}
+function v(){
+    document.write("<center>")
+    document.write("Viral " + "<br>" + "Velvet" + "<br>" + "Vital" + "<br>" +"Violet")
+    document.write("</center>")
+}
+function w(){
+    document.write("<center>")
+    document.write("Woman"+ "<br>" + "White" + "<br>" + "Water" + "<br>" +"Whale")
+    document.write("</center>")
+}
+function x(){
+    document.write("<center>")
+    document.write("Xbox"+ "<br>" +"X-ray")
+    document.write("</center>")
+}
+function y(){
+    document.write("<center>")
+    document.write("Yell"+ "<br>" + "Yawn" + "<br>" + "Yoga" + "<br>" +"Yard")
+    document.write("</center>")
+}
+function z(){
+    document.write("<center>")
+    document.write("Zoology"+ "<br>" + "Zone" + "<br>" + "Zigzag" + "<br>"+ "Zoo")
+    document.write("</center>")
+}
